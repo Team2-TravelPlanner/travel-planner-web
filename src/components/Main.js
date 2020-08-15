232cf3c411c33380bff4fd43fda0170f7865372d
Jennifer Chen <shinyi.chen@gmail.com>
2020-08-15 06:53:14 -0700

    Fixed className

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Main extends React.Component {
   render() {
     return (
-      <div class="main">
+      <div className="main">
         Main
       </div>
     )
