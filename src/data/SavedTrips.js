104389a48b5ba84194f23798aded50157d0175fa
Jennifer Chen <shinyi.chen@gmail.com>
2020-08-17 11:13:55 -0700

    Fake data for testing UI

---
+++ src/data/SavedTrips.js
@@ -0,0 +1,14 @@
+const savedTrips = [
+  {
+  id: "1",
+  name: "My NY trip 1",
+  startDate: "1608451200000",
+  endDate: "1608883200000"
+  },
+  {
+    id: "2",
+    name: "My NY trip 2",
+    startDate: "1620198000000",
+    endDate: "1620370800000"
+  }
+]
