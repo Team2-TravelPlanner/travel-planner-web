60b433c9e225c99e11f21dd1b8943da29d51e1c6
HX001 <hongxu2015@gmail.com>
2020-08-18 21:02:06 -0400

    Draft Implementation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import {Switch, Route, Redirect} from "react-router-dom";
+import Plans from "./Plans"
 
 class Main extends React.Component {
   render() {
     return (
-      <div class="main">
-        Main......
+      <div className="main">
+        <switch>
+            <Route path="/plans" render={this.getPlanDisplay}/>
+        </switch>
       </div>
     )
   }
+    getPlanDisplay = () => {
+        // return this.props.isLoggedIn ?
+        //     <Redirect to="/plansDisplay"/> : <Login handleLoginSucceed={this.props.handleLoginSucceed}/>
+        return <Plans />
+    }
 }
 
 export default Main;
