393c8b08d1462c20c91b08eb6c8f8eb032e8d123
Tiansheng Sun <suntiansheng2015@126.com>
2020-08-25 21:21:25 -0400

    Map Component
    Save Map component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,64 +1,16 @@
-import React from 'react';
-import { Switch, Route } from "react-router-dom";
-import Home from "./Home";
-import Form from "./Form";
-import NotFound from "./NotFound";
-import { Modal } from "react-bootstrap";
+import Map from "./NewYorkMap"
+import locations from "../data/SearchResults"
 
 class Main extends React.Component {
 
-  state = {
-    showForm: false
-  }
-
-  openForm = () => {
-    this.setState({
-      showForm: true
-    });
-  }
-
-  closeForm = () => {
-    this.setState({
-      showForm: false
-    });
-  }
-
-  generateItinerary = (options) => {
-    this.setState({
-      showForm: false
-    });
-
-    console.log(options);
-  }
-
-  render() {
-    const { showForm } = this.state;
-
-    return (
-      <div className="main">
-        <Switch>
-          <Route exact path="/">
-            <Home openForm={this.openForm} />
-          </Route>
-          <Route exact path="/explorer">
-            <NotFound />
-          </Route>
-          <Route path="/">
-            <NotFound />
-          </Route>
-        </Switch>
-
-        <Modal
-          show={showForm}
-          backdrop="static"
-          keyboard={false}>
-          <Modal.Body>
-            <Form close={this.closeForm} generateItinerary={this.generateItinerary} />
-          </Modal.Body>
-        </Modal>
-
-
-      </div>
+    render() {
+        return (
+            <Map
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3CEh9DXuyjozqptVB5LA-dN7MxWWkr9s&v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%`}} />}
+                containerElement={<div style={{ height: `600px` }} />}
+                mapElement={<div style={{ height: `100%`}} />}
+            />
     )
   }
 }
---
+++ src/components/PlaceMarkers.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Marker, InfoWindow } from 'react-google-maps';
+import PropTypes from 'prop-types';
+
+class AroundMarker extends React.Component {
+    static propTypes = {
+        place: PropTypes.object.isRequired,
+    }
+
+    state = {
+        isOpen: false,
+    }
+
+    handleToggle = () => {
+        this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+        // if (this.props.map.zoom < 13) {
+        //     this.props.map.zoom = 13;
+        // }
+
+    }
+
+    // zoom = () => {
+    //     if (zoom < 13) {
+    //         setZoom(13);
+    //     }
+    // }
+
+    render() {
+        const { address, message, imageUrl, category, info, lat, lon } = this.props.place;
+        return (
+            <Marker
+                position={{ lat:lat, lng: lon }}
+                // onMouseOver={this.handleToggle}
+                // onMouseOut={this.handleToggle}
+                onClick={this.handleToggle}
+            >
+                {this.state.isOpen ? (
+                    <InfoWindow>
+                        <div>
+                            <img src={imageUrl} alt={message} className="around-marker-image"/>
+                            <p>{`address: ${address}`}</p>
+                            <p>{`category: ${category}`}</p>
+                            <p>{`info: ${info}`}</p>
+                        </div>
+                    </InfoWindow>
+                ) : null}
+            </Marker>
+        );
+    }
+}
+
+export default AroundMarker;
