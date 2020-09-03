fe41458fbc6791b73d87d3b8e2d8f28b7c84c9eb
Jennifer Chen <shinyi.chen@gmail.com>
2020-09-03 13:31:47 -0700

    Use different user icon color when logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -64,10 +64,23 @@ class Header extends React.Component {
         <div className="DropDown">
           <Dropdown>
             <Dropdown.Toggle as={this.customToggle} id="dropdown-custom-components">
-              <img className="avatar"
+              {/* <img className="avatar"
                 src={avatar}
                 alt="user pic"
-              />
+              /> */}
+              {this.props.isLoggedIn?
+                <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-person-circle avatar-LoggedIn" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+                  <path fillRule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                  <path fillRule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+                </svg>
+                : 
+                <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-person-circle avatar" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+                  <path fillRule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                  <path fillRule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+                </svg>
+            }
             </Dropdown.Toggle>
             <Dropdown.Menu>
               {this.showMenu()}
