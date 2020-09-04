9d07023097874f9da8ce215921cc2e37dc25cd52
Jennifer Chen <shinyi.chen@gmail.com>
2020-09-04 15:33:14 -0700

    Hide API key

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const TOKEN_KEY = 'TOKEN_KEY';
 export const ID = 'ID';
 export const URL = 'http://localhost:8080';
 export const GeocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json";
-export const GoogleKey = "AIzaSyCvGaWEauH2tP14Mi9xO-NUj0f0p78cgzY";
+export const GoogleKey = process.env.REACT_APP_GOOGLE_API_KEY;
