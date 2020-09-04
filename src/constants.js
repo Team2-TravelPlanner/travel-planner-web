58b0244a693c07c684082fdd8640cd723ac9e8d5
Jennifer Chen <shinyi.chen@gmail.com>
2020-09-03 21:25:05 -0700

    Update API key

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const TOKEN_KEY = 'TOKEN_KEY';
 export const ID = 'ID';
 export const URL = 'http://localhost:8080';
 export const GeocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json";
-export const GoogleKey = "AIzaSyAuciDA139jQsnn9INVNKzXVHgYOzl889s";
+export const GoogleKey = "AIzaSyCvGaWEauH2tP14Mi9xO-NUj0f0p78cgzY";
